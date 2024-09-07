d83b9ef8950668aab7f3a1c76ff6a9c1edaf3113
Anuj Kumar <anuj9654949185@gmail.com>
2024-09-07 19:18:15 +0530

    completed update functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { createListing, deleteListing } from '../controllers/listing.controller.js'
+import { createListing, deleteListing, getListing, updateListing } from '../controllers/listing.controller.js'
 import {verifyToken} from '../utils/VerifiedUser.js'
 
 const router = express.Router()
 
 router.post('/create' , verifyToken, createListing)
 router.delete('/delete/:id', verifyToken, deleteListing)
+router.post('/update/:id', verifyToken, updateListing)
+router.get('/get/:id', getListing)
 
 export default router
