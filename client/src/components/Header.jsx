e713a083714900d08cf1fe2906eaef225aa72292
Anuj Kumar <anuj9654949185@gmail.com>
2024-09-09 12:18:09 +0530

    name change

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,8 +9,8 @@ export default function Header() {
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
             <Link to='/'>
                 <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-                    <span className='text-slate-500'>Opus</span>
-                    <span className='text-slate-700'>Living</span>
+                    <span className='text-slate-500'>Anuj</span>
+                    <span className='text-slate-700'>Estate</span>
                 </h1>
             </Link>
             <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
